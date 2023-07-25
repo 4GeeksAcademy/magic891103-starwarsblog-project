3a5285903bd6fef7bee433087c824b016f1f2912
magic891103 <133174388+magic891103@users.noreply.github.com>
2023-07-25 00:02:43 +0000

    some changes

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,46 +6,45 @@ import { Vehicles } from "../component/Vehicles.jsx";
 import { Planets } from "../component/Planets.jsx";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context)
+  const { store, actions } = useContext(Context);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			actions.getCharacterData();
-			actions.getVehiclesData();
-			actions.getPlanetsData();
-		};
-		fetchData();
-	}, []);
-	
-	return (
-		<div className="container">
-		<div className="inline-scroll">
-			<div className="row">
-			<h1 className="text-danger">Characters</h1>
-				<div className="text-center mt-5 d-flex overflow-x-auto">
-					{store.characters.map(el =>	
-					<Character key={el.uid} el={el} />)}
-				</div>
-				</div>
-			</div>	
-			<div className="inline-scroll">
-			<div className="row">
-			<h1 className="text-danger">Vehicles</h1>
-				<div className="text-center mt-5 d-flex overflow-x-auto">
-					{store.vehicles.map(el =>
-					<Vehicles key={el.uid} el={el} />)}
-						</div>
-				</div>
-			</div>
-			<div className="inline-scroll">
-			<div className="row">
-			<h1 className="text-danger">Planets</h1>
-				<div className="text-center mt-5 d-flex overflow-x-auto">
-					{store.planets.map(el => 
-					<Planets key={el.uid} el={el} />)}
-						</div>
-				</div>
-			</div>
-		</div>
-	);
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      actions.getCharacterData();
+      actions.getVehiclesData();
+      actions.getPlanetsData();
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="scroll-container">
+        <div className="row">
+          <h1 className="text-danger">Characters</h1>
+          <div className="text-center mt-5 d-flex overflow-x-auto">
+            {store.characters.map((el) => (
+              <Character key={el.uid} el={el} />
+            ))}
+          </div>
+        </div>
+        <div className="row">
+          <h1 className="text-danger">Vehicles</h1>
+          <div className="text-center mt-5 d-flex overflow-x-auto">
+            {store.vehicles.map((el) => (
+              <Vehicles key={el.uid} el={el} />
+            ))}
+          </div>
+        </div>
+        <div className="row">
+          <h1 className="text-danger">Planets</h1>
+          <div className="text-center mt-5 d-flex overflow-x-auto">
+            {store.planets.map((el) => (
+              <Planets key={el.uid} el={el} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
